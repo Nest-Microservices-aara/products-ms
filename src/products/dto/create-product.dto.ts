69e3202e9d8498459206f83ca392e81ef8af8f21
Arturo Alcázar Rodríguez <alcazararturo@hotmail.com>
2024-05-03 19:50:30 -0600

    first commit

---
+++ src/products/dto/create-product.dto.ts
@@ -0,0 +1,20 @@
+import {Type} from 'class-transformer';
+import { IsBoolean, IsNumber, IsOptional, IsString, Min } from 'class-validator';
+
+export class CreateProductDto {
+    @IsString()
+    id: string;
+    @IsString()
+    name        : string;
+    @IsString()
+    description: string;
+    @IsNumber({
+        maxDecimalPlaces: 4,
+    })
+    @Min(0)
+    @Type( () => Number )
+    price       : number;
+    @IsBoolean()
+    available : boolean;
+
+}
